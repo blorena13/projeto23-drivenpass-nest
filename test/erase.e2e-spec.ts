ab40f84a49a8f3acb7d008c043957222ed97d746
Lorena <blorena1321@gmail.com>
2023-09-04 11:17:22 -0300

    mudanças 4

---
+++ test/erase.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { PrismaService } from "../src/prisma/prisma.service";
+import { JwtService } from "@nestjs/jwt";
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppModule } from "../src/app.module";
+
+describe('credentials tests', () => {
+    let app: INestApplication;
+    let prisma: PrismaService = new PrismaService();
+    let jwt:JwtService=new JwtService({secret:process.env.JWT_SECRET})
+
+    beforeEach(async ()=> {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule]
+        })
+        .overrideProvider(PrismaService)
+        .useValue(prisma)
+        .compile();
+
+        app = moduleFixture.createNestApplication()
+        app.useGlobalPipes(new ValidationPipe())
+        await app.init();
+
+       await prisma.credentials.deleteMany();
+       await prisma.notes.deleteMany();
+       await prisma.card.deleteMany();
+       await prisma.user.deleteMany();
+       
+    })
+
+    afterAll(async () => {
+        await app.close();
+        await prisma.$disconnect();
+    });
+
+    it('POST /cards => should create card', async () => {
+
+    })
+})
